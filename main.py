59da04725bec42b9c90bfdcfad3915cfe68f5bdd
EstructuradeDatosClII <ciclodos844@gmail.com>
2024-06-14 19:58:45 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from fundamentos import ejemplos
+
+mensaje = input("Ingrese un mensaje:")
+ejemplos.mayusculaMinuscula(mensaje)
+ejemplos.longitudMensaje(mensaje)
+ejemplos.convertirNumberString(mensaje)
+ejemplos.eliminarEspacios(mensaje)
+ejemplos.arregloPalabras(mensaje)
